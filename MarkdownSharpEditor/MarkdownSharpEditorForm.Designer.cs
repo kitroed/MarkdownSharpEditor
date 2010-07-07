8673b6a018a99a24c693ac25b260b735556734d2
Kit Roed <kit.roed@gmail.com>
2010-07-07 05:38:53 -0500

    Added "Open", "Save", and "Save As" menu items to the File menu.  Added open dialog box and save dialog box to form.

--- MarkdownSharpEditor/MarkdownSharpEditorForm.Designer.cs
+++ MarkdownSharpEditor/MarkdownSharpEditorForm.Designer.cs
@@ -31,16 +31,21 @@
             this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
             this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.alternateHorizontalVerticalLayoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
             this.outputTabControl = new System.Windows.Forms.TabControl();
             this.htmlOutputTabPage = new System.Windows.Forms.TabPage();
             this.outputWebBrowser = new System.Windows.Forms.WebBrowser();
             this.rawOutputTabPage = new System.Windows.Forms.TabPage();
             this.outputRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.mainMenuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
             this.mainSplitContainer.Panel1.SuspendLayout();
@@ -73,17 +78,45 @@
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveFileToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.openFileToolStripMenuItem.Text = "&Open...";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.saveFileToolStripMenuItem.Text = "&Save";
+            this.saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift)
+                        | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.saveAsToolStripMenuItem.Text = "Save &As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // viewToolStripMenuItem
@@ -91,14 +124,14 @@
             this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.alternateHorizontalVerticalLayoutToolStripMenuItem});
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            this.viewToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
-            this.viewToolStripMenuItem.Text = "View";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "&View";
             // 
             // alternateHorizontalVerticalLayoutToolStripMenuItem
             // 
             this.alternateHorizontalVerticalLayoutToolStripMenuItem.Name = "alternateHorizontalVerticalLayoutToolStripMenuItem";
-            this.alternateHorizontalVerticalLayoutToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
-            this.alternateHorizontalVerticalLayoutToolStripMenuItem.Text = "Alternate Horizontal/Vertical Layout";
+            this.alternateHorizontalVerticalLayoutToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
+            this.alternateHorizontalVerticalLayoutToolStripMenuItem.Text = "Alternate Horizontal/Vertical &Layout";
             this.alternateHorizontalVerticalLayoutToolStripMenuItem.Click += new System.EventHandler(this.alternateHorizontalVerticalLayoutToolStripMenuItem_Click);
             // 
             // mainSplitContainer
@@ -118,6 +151,23 @@
             this.mainSplitContainer.Size = new System.Drawing.Size(603, 670);
             this.mainSplitContainer.SplitterDistance = 371;
             this.mainSplitContainer.TabIndex = 2;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.AcceptsReturn = true;
+            this.inputTextBox.AcceptsTab = true;
+            this.inputTextBox.AllowDrop = true;
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(603, 368);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
             // 
             // outputTabControl
             // 
@@ -180,22 +230,14 @@
             this.outputRichTextBox.TabIndex = 0;
             this.outputRichTextBox.Text = "";
             // 
-            // inputTextBox
+            // openFileDialog
             // 
-            this.inputTextBox.AcceptsReturn = true;
-            this.inputTextBox.AcceptsTab = true;
-            this.inputTextBox.AllowDrop = true;
-            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.inputTextBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.inputTextBox.Location = new System.Drawing.Point(0, 0);
-            this.inputTextBox.Multiline = true;
-            this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.inputTextBox.Size = new System.Drawing.Size(603, 368);
-            this.inputTextBox.TabIndex = 0;
-            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            this.openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog.Title = "Select a text file";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
             // 
             // MarkdownSharpEditorForm
             // 
@@ -238,6 +280,11 @@
         private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem alternateHorizontalVerticalLayoutToolStripMenuItem;
         private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
 
--- MarkdownSharpEditor/MarkdownSharpEditorForm.cs
+++ MarkdownSharpEditor/MarkdownSharpEditorForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
 using System.Diagnostics;
 
@@ -13,6 +14,8 @@ namespace MarkdownEditor
     public partial class MarkdownSharpEditorForm : Form
     {
         MarkdownSharp.Markdown markdown = new MarkdownSharp.Markdown();
+
+        private string filePath;
 
         public MarkdownSharpEditorForm()
         {
@@ -37,22 +40,72 @@ namespace MarkdownEditor
             int width = Width;
             int height = Height;
 
-            Width = height;
-            Height = width;
+
 
             if (mainSplitContainer.Orientation == System.Windows.Forms.Orientation.Horizontal)
             {
+                Width = height;
+                Height = width;
+
                 mainSplitContainer.Orientation = System.Windows.Forms.Orientation.Vertical;
             }
             else
             {
                 mainSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+
+                Width = height;
+                Height = width;
             }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+            }
+
+            if (filePath != null)
+            {
+                inputTextBox.Text = File.ReadAllText(filePath);
+            }
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // if there is already a filePath, skip dialog.
+            if (filePath == null)
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = saveFileDialog.FileName;
+                }
+            }
+
+            if (filePath != null)
+            {
+                File.WriteAllText(filePath, inputTextBox.Text);
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.FileName = filePath;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = saveFileDialog.FileName;
+            }
+
+            if (filePath != null)
+            {
+                File.WriteAllText(filePath, inputTextBox.Text);
+            }
         }
     }
 }
