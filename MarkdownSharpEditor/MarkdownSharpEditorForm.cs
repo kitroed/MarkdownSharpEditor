126b14fa5a7ef7c8b5bd506bb064d938d1f26763
Kit Roed <kit.roed@gmail.com>
2010-07-19 05:05:21 +0000

    Added a default style to the rendered html.  Storing style and file path in public properties.

--- MarkdownSharpEditor/MarkdownSharpEditorForm.cs
+++ MarkdownSharpEditor/MarkdownSharpEditorForm.cs
@@ -15,7 +15,15 @@ namespace MarkdownEditor
     {
         MarkdownSharp.Markdown markdown = new MarkdownSharp.Markdown();
 
-        private string filePath;
+        public string FilePath { get; set; }
+        public string Style
+        {
+            get
+            { return _style; }
+            set
+            { _style = value; }
+        }
+        private string _style = "<style>html,body,table{font: normal 10pt Verdana;}li{margin-bottom:7px;}h1{font-size:18pt;}h2{font-size:14pt;}</style>";
 
         public MarkdownSharpEditorForm()
         {
@@ -31,7 +39,7 @@ namespace MarkdownEditor
 
             string outputHtml = markdown.Transform(inputTextBox.Text);
 
-            outputWebBrowser.DocumentText = outputHtml;
+            outputWebBrowser.DocumentText = Style + outputHtml;
             outputRichTextBox.Text = outputHtml;
         }
 
@@ -67,52 +75,52 @@ namespace MarkdownEditor
         {
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                filePath = openFileDialog.FileName;
+                FilePath = openFileDialog.FileName;
             }
 
-            if (filePath != null)
+            if (FilePath != null)
             {
-                inputTextBox.Text = File.ReadAllText(filePath);
+                inputTextBox.Text = File.ReadAllText(FilePath);
             }
         }
 
         private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // if there is already a filePath, skip dialog.
-            if (filePath == null)
+            if (FilePath == null)
             {
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    filePath = saveFileDialog.FileName;
+                    FilePath = saveFileDialog.FileName;
                 }
             }
 
-            if (filePath != null)
+            if (FilePath != null)
             {
-                File.WriteAllText(filePath, inputTextBox.Text);
+                File.WriteAllText(FilePath, inputTextBox.Text);
             }
         }
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            saveFileDialog.FileName = filePath;
+            saveFileDialog.FileName = FilePath;
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                filePath = saveFileDialog.FileName;
+                FilePath = saveFileDialog.FileName;
             }
 
-            if (filePath != null)
+            if (FilePath != null)
             {
-                File.WriteAllText(filePath, inputTextBox.Text);
+                File.WriteAllText(FilePath, inputTextBox.Text);
             }
         }
 
         private void reloadDocumentToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (filePath != null)
+            if (FilePath != null)
             {
-                inputTextBox.Text = File.ReadAllText(filePath);
+                inputTextBox.Text = File.ReadAllText(FilePath);
             }
         }
     }
